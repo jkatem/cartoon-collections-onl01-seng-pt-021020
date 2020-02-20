23a8521edcec7e820a9cc6ae420a16240e2006b6
Jen Miao <jenmiao@gmail.com>
2020-02-20 00:54:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   array.each_with_index do | name , index |
-    puts "#{index + 1}. #{name}"
+    index += 1
+    puts "#{index}. #{name}"
   end
 end
 
