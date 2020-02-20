c4dbcbc70e92e4bc46ecf708d941cc7986d67277
Jen Miao <jenmiao@gmail.com>
2020-02-20 01:34:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,13 +15,14 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)
-    #newArr = []
+    
     array.map! { |name| name.capitalize + "!"}
-
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |x|
+    x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
